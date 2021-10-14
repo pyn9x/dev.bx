4c128826659cd201c1cdb63f07b2ab0d1450a471
Грошев Максим <an9ous8@mail.ru>
2021-10-14 15:59:54 +0200

    Создал ветку homework-2, где выполнил домашнее задание

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
+/** @var array $movies */
+require('movies.php');
 
-echo "Hello, World";
+echo "Welcome to move list!
+Enter age: ";
+$input = readline(" \n");
+$index = 1;
+
+if(is_numeric($input) && (int)$input >= 0)
+{
+	foreach ($movies as $movie)
+	{
+		if($input >= $movie['age_restriction'])
+		{
+			echo "{$index}. {$movie['title']} ({$movie['release_year']}), {$movie['age_restriction']}. Rating - {$movie['rating']}\n";
+			$index++;
+		}
+	}
+}else echo "Wrong age";
+
+
+
+
