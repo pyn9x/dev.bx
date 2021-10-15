91e94175f529abce2ea792a91b787acd9e17890c
Грошев Максим <an9ous8@mail.ru>
2021-10-15 17:10:54 +0200

    Переделал неявноые приведения типов, испрвил формат блока else и исправил опечатку "move list"

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /** @var array $movies */
 require('movies.php');
 
-echo "Welcome to move list!
+echo "Welcome to movie list!
 Enter age: ";
 $input = readline(" \n");
 $index = 1;
@@ -11,13 +11,17 @@ if(is_numeric($input) && (int)$input >= 0)
 {
 	foreach ($movies as $movie)
 	{
-		if($input >= $movie['age_restriction'])
+		$age_restriction = (int)$movie['age_restriction'];
+		if($input >= $age_restriction)
 		{
 			echo "{$index}. {$movie['title']} ({$movie['release_year']}), {$movie['age_restriction']}. Rating - {$movie['rating']}\n";
 			$index++;
 		}
 	}
-}else echo "Wrong age";
+}else
+{
+	echo "Wrong age";
+}
 
 
 
